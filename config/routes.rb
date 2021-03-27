7aa0a44a5300011fdf00f43836c1ec29f9074adb
Michael Knott <knottweb@gmail.com>
2021-03-27 00:17:38 +0000

    amend routes to required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :trainer
-  resources :pokemon
+  resources :trainer, only: [:show, :new, :create, :edit, :update]
+  resources :pokemon, only: [:index, :show, :new, :create, :edit, :update]
 end
