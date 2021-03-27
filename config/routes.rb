19ee4235fc313ad6a7d14a7b82a799866dda40ad
Michael Knott <knottweb@gmail.com>
2021-03-27 01:38:21 +0000

    move navbar from homepage to shared partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#homepage"
   get 'homepage', to: 'pages#homepage'
-  resources :trainer, only: [:show, :new, :create, :edit, :update]
-  resources :pokemon, only: [:index, :show, :new, :create, :edit, :update]
+  resources :trainers, only: [:show, :new, :create, :edit, :update]
+  resources :pokemons, only: [:index, :show, :new, :create, :edit, :update]
 end
