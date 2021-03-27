dc2f8b3a21e2b7c7e3d8b898575f4f4fd687b6f2
Michael Knott <knottweb@gmail.com>
2021-03-27 00:45:33 +0000

    add route for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "pages#homepage"
+  get 'homepage', to: 'pages#homepage'
   resources :trainer, only: [:show, :new, :create, :edit, :update]
   resources :pokemon, only: [:index, :show, :new, :create, :edit, :update]
 end
