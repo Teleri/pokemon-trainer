f5f2f1036c16ac4a239037b8f576850f329d1503
Michael Knott <knottweb@gmail.com>
2021-05-08 12:22:25 +0100

    fix navbar and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# t.string "name"
+# t.integer "age"
+# t.string "gender"
+# t.string "favourite_pokemon"
+# t.string "hometown"
+
+10.times do
+  Trainer.create(name: "Test Trainer", age: 20, gender: "male", favourite_pokemon: "Test Pokemon", hometown: "Hometown")
+end
